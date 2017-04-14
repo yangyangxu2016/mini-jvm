e8051cf1015289b14f74bc3188c4a30901eeef60
徐阳阳 <1425809544@qq.com>
2017-04-14 17:46:05 +0800

    中序表达式作业

---
+++ src/main/java/com/coding/basic/stack/expr/InfixExpr.java
@@ -0,0 +1,20 @@
+package com.coding.basic.stack.expr;
+
+/**
+ * @author xyy
+ * @create 2017-04-14 17:44
+ **/
+public class InfixExpr {
+
+    String expr = null;
+
+    public InfixExpr(String expr) {
+        this.expr = expr;
+    }
+
+    public float evaluate() {
+
+        return 0.0f;
+    }
+
+}
