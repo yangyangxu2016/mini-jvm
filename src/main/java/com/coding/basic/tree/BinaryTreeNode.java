28d28b55334ad5dda203b964b5d4cca1fc580136
徐阳阳 <1425809544@qq.com>
2017-06-14 17:42:20 +0800

    二叉树的实现插入和搜索

--- src/main/java/com/coding/basic/tree/BinaryTreeNode.java
+++ src/main/java/com/coding/basic/tree/BinaryTreeNode.java
@@ -1,24 +1,24 @@
 package com.coding.basic.tree;
 
 /**
- * 二叉树
+ * 二叉树的实现插入和搜索
  *
  * @author xyy
- * @create 2017-05-09 19:42
+ * @create 2017-06-14 17:11
  **/
-public class BinaryTreeNode<T> {
+public class BinaryTreeNode<T extends Comparable> {
 
-
-    private  T data;
-    private BinaryTreeNode<T> left;
-    private BinaryTreeNode<T> right;
+    private T data;
+    private BinaryTreeNode left;
+    private BinaryTreeNode right;
+    private int size;
 
 
     public T getData() {
         return data;
     }
 
-    public BinaryTreeNode setData(T data) {
+    public BinaryTreeNode<T> setData(T data) {
         this.data = data;
         return this;
     }
@@ -27,7 +27,7 @@ public class BinaryTreeNode<T> {
         return left;
     }
 
-    public BinaryTreeNode setLeft(BinaryTreeNode<T> left) {
+    public BinaryTreeNode<T> setLeft(BinaryTreeNode<T> left) {
         this.left = left;
         return this;
     }
@@ -36,18 +36,65 @@ public class BinaryTreeNode<T> {
         return right;
     }
 
-    public BinaryTreeNode setRight(BinaryTreeNode<T> right) {
+    public BinaryTreeNode<T> setRight(BinaryTreeNode<T> right) {
         this.right = right;
         return this;
     }
 
 
-    public BinaryTreeNode(T data) {
-        this.data = data;
+    public BinaryTreeNode<T> insert(T data) {
+        if (this.data == null) {
+            this.data = data;
+            return this;
+        }
+        int comparaResult = this.data.compareTo(data);
+        if (comparaResult > 0) {
+            if (this.left == null) {
+                this.left = new BinaryTreeNode();
+                this.left.data = data;
+                return this.left;
+            } else {
+                return this.left.insert(data);
+            }
+        } else if (comparaResult < 0) {
+            if (this.right == null) {
+                this.right = new BinaryTreeNode();
+                this.right.data = data;
+                return this.right;
+            } else {
+                return this.right.insert(data);
+            }
+        } else {
+            return this;
+        }
     }
 
-    public BinaryTreeNode<T> insert(Object o) {
-        return null;
-    }
 
+    public BinaryTreeNode<T> search(T data) {
+        if (this.data == null) {
+            return null;
+        }
+        int comparaResult = this.data.compareTo(data);
+        if (comparaResult > 0) {
+            if (this.left == null) {
+                return null;
+            } else {
+                return this.left.search(data);
+            }
+        } else if (comparaResult < 0) {
+            if (this.right == null) {
+                return null;
+            } else {
+                return this.right.search(data);
+            }
+        } else {
+            return this;
+        }
+
+    }
 }
+
+
+
+
+
